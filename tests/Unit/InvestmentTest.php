9fa190c8c4fd9e43071588cf8e37cf96ff7d7b6b
Rich Jones <jones_rich@yahoo.com>
2018-12-09 11:20:28 -0600

    completed Investment Tests; next up Offering Tests

---
+++ tests/Unit/InvestmentTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Unit;
+
+use App\Domain\Investment;
+use App\Validation\PaymentMethod;
+use PHPUnit\Framework\TestCase;
+
+class InvestmentTest extends TestCase
+{
+    /** @var int */
+    const myAmount = 1000;
+    /** @var string */
+    const myPaymentMethod = PaymentMethod::CASH;
+
+    /** @var Investment */
+    private $investment;
+
+    /**
+     * @throws \Exception
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setInvestment(
+            new Investment(
+                self::myAmount,
+                 new PaymentMethod(self::myPaymentMethod)
+            )
+        );
+    }
+
+    public function test_amount_constructed()
+    {
+        $this->assertSame($this->getInvestment()->getAmount(), self::myAmount);
+    }
+
+    public function test_payment_method_constructed()
+    {
+        $this->assertSame($this->getInvestment()->getPaymentMethod(), self::myPaymentMethod);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function test_none_numeric_amount()
+    {
+        $this->expectExceptionMessage('must be of the type integer, string given,');
+
+        /* @noinspection PhpStrictTypeCheckingInspection */
+        new Investment(
+            'bob',
+            new PaymentMethod(self::myPaymentMethod)
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function test_none_PaymentMethod_type()
+    {
+        $this->expectExceptionMessage('not a valid payment method');
+
+        /* @noinspection PhpStrictTypeCheckingInspection */
+        new Investment(
+            self::myAmount,
+            new PaymentMethod('bob')
+        );
+    }
+
+    /**
+     * @return Investment
+     */
+    public function getInvestment(): Investment
+    {
+        return $this->investment;
+    }
+
+    /**
+     * @param Investment $investment
+     *
+     * @return InvestmentTest
+     */
+    private function setInvestment(Investment $investment): self
+    {
+        $this->investment = $investment;
+
+        return $this;
+    }
+}
